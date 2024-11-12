49d3f4af16694548d13b5387f0f076ddf4dd5126
mdenitti <massimodn77@gmail.com>
2024-11-12 13:53:45 +0100

    added reset button and output iteration within the fetch

--- fetch.js
+++ fetch.js
@@ -1,11 +1,24 @@
 const url = 'https://jsonplaceholder.typicode.com/todos';
-fetch(url)
-.then(res => res.json())
-.then(data => {
-    
-})
-.catch(e => console.log(e));
+const output = document.getElementById('output');
 
+function fetchdata() {
+    fetch(url)
+    .then(res => res.json())
+    .then(
+        data => data.forEach(smurf => {
+            output.innerHTML += `<li>${smurf.title}</li>`;
+        })
+    )
+    .catch(e => console.log(e));
+}
+fetchdata();
+
+const button = document.getElementById('clear');
+button.addEventListener('click', () => {
+    output.innerHTML = '';
+})
+
+console.log(data)
 // HTTP VERB (GET) (POST,DELETE,PUT)
 
 // SUBPROBLEMS
@@ -14,7 +27,7 @@ fetch(url)
 // create a JS file with logic
 // find endpoint api
 // consume endpoint api
-// display data in HTML using a iteration
+// display data in HTML using an iteration
         // make iteration
         // create HTML node (id)
         // node.innerHTML (+=)
