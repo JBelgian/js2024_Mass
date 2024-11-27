595a03b1d75ea41549f4c8a22503c015b77dbed6
mdenitti <massimodn77@gmail.com>
2024-11-27 10:32:12 +0100

    taxi oop public attributes / methods

---
+++ advanced/taxi.js
@@ -0,0 +1,69 @@
+// taxi class definition
+class Taxi {
+    constructor(make, model, client) {
+        this.make = make; // Make of the taxi
+        this.model = model; // Model of the taxi
+        this.totalDistance = 0; // Total distance traveled in KM
+        this.farePerKm = 2.6;
+        this.fare = 0;
+        this.startFee = 3.5;
+        this.client = client;
+    }
+
+    // Method to start a new fare
+    startFare() {
+        this.totalDistance = 0;
+        this.fare = 0;
+        console.log(`Trip started in ${this.model} / ${this.make}`)
+    }
+
+    // Method to add distance traveled
+    addDistance(distance) {
+        if (distance < 0) {
+            console.log('Distance cannot be negative');
+            return // exit the method and stop the rest of the execution
+        }
+        this.totalDistance += distance;
+        console.log (`Added ${distance} km. Total distance: ${this.totalDistance} km.`);
+    }
+
+    // Method to calculate the total fare
+    calculateTotalFare () {
+        this.fare = this.fare + this.startFee;
+        this.fare += this.farePerKm * this.totalDistance;
+        console.log(`Total fare for ${this.totalDistance} km. is ${this.fare.toFixed(2)} EUR`);
+        return this.fare;
+    }
+
+    endTrip() {
+        console.log(`Trip ended! Thank you come again. Total fare: ${this.fare.toFixed(2)}`)
+        // send email to thank the client... for example/plausible routine
+    }
+}
+
+class Client {
+    constructor (name,phone,email){
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+}
+
+// Create a new Taxi object
+const myClient = new Client('Massimo','0000000');
+const myTaxi = new Taxi('Toyota', 'Corolla', myClient);
+
+console.log(myClient);
+
+// start a new fare
+myTaxi.startFare();
+
+// Add distance traveled
+myTaxi.addDistance(12);
+myTaxi.addDistance(60);
+
+// Calculate and display total fare
+myTaxi.calculateTotalFare();
+
+// End the trip and reset
+myTaxi.endTrip();
